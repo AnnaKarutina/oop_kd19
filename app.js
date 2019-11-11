32f84fb7543f2ad1fce614db5218232fd6e94208
Anna Karutina <anna.karutina@khk.ee>
2019-11-11 11:23:50 +0200

    wondow meetodid

---
+++ app.js
@@ -0,0 +1,16 @@
+// window objects / methods / properties
+
+// alert
+// window.alert('Tere Hommikust!');
+// alert('Tere Hommikust!');
+
+// propt
+const sisend = prompt('Sisesta oma nimi');
+alert('Tere Hommikust, ' + sisend + '!');
+
+// confirm
+if(confirm('Kas oled nõus?')){
+  console.log('jah');
+} else {
+  console.log('ei');
+}
