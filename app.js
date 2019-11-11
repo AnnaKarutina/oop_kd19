7bfb04322f3d0d8778cb6b9deab19508f5e23a44
Anna Karutina <anna.karutina@khk.ee>
2019-11-11 11:34:37 +0200

    window omadused

--- app.js
+++ app.js
@@ -5,12 +5,27 @@
 // alert('Tere Hommikust!');
 
 // propt
-const sisend = prompt('Sisesta oma nimi');
-alert('Tere Hommikust, ' + sisend + '!');
+// const sisend = prompt('Sisesta oma nimi');
+// alert('Tere Hommikust, ' + sisend + '!');
 
 // confirm
-if(confirm('Kas oled nõus?')){
-  console.log('jah');
-} else {
-  console.log('ei');
-}
+// if(confirm('Kas oled nõus?')){
+//   console.log('jah');
+// } else {
+//   console.log('ei');
+// }
+
+// välimõõdud
+let valiKorgus = window.outerHeight;
+let valiLaius = window.outerWidth;
+// console.log(valiLaius)
+
+// sisemõõdud
+let siseKorgus = window.innerHeight;
+let siseLaius = window.innerWidth;
+// console.log(siseLaius)
+
+// scroll
+scrolY = window.scrollY;
+scrolX = window.scrollX;
+console.log(scrolY);
