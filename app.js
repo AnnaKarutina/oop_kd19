a0331b1f77bc20e5a62def0ce6fa78a6eca61941
Anna Karutina <anna.karutina@khk.ee>
2019-11-12 15:45:05 +0200

    document.querySelector() stiili muutmine

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 // document meetodid
 // single element selectors
 
-sisu = document.getElementById('title'); // html sisu
-id = document.getElementById('title').id; // id
-klass = document.getElementById('title').className;
+sisu = document.querySelector('#title'); // id järgi
+sisu = document.querySelector('.card-title'); // klassi nime järgi
+sisu = document.querySelector('h5'); // elemendi järgi
 
 // stiili muutmine
-// sisu.style.background = '#333';
-document.getElementById('title').style.background = '#333';
-sisu.style.color = '#fff';
-sisu.style.padding = '5px';
-// sisu.style.display = 'none';
-
-// sisu muutmine
-sisu.textContent = 'Minu ülesanded';
-sisu.innerText = 'Just minu ülesanded';
-sisu.innerHTML = '<span style="color:lightgreen">Vabad ülesanded</span>';
+document.querySelector('li').style.color = 'red';
+document.querySelector('ul li').style.color = 'blue';
+document.querySelector('li:last-child').style.color = 'red';
+document.querySelector('li:nth-child(2)').style.color = 'orange';
 console.log(sisu);
