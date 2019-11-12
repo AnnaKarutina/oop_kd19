f77df97ec7f5c25c3e4e28c0ac33a520b99e92db
Anna Karutina <anna.karutina@khk.ee>
2019-11-12 15:33:39 +0200

    document kaudu stiili muutmine

--- app.js
+++ app.js
@@ -1,39 +1,14 @@
-// document
+// document meetodid
+// single element selectors
 
-let sisu;
+sisu = document.getElementById('title'); // html sisu
+id = document.getElementById('title').id; // id
+klass = document.getElementById('title').className;
 
-sisu = document; // sisu html-is
-sisu = document.all; // html tagid kasutamisjärjekorras
-sisu = document.all[2]; // konkreetne html tag
-sisu = document.all.length; // palju on html tagi kasutusel
-
-// document properties - oobjekti omadused
-sisu = document.head; // head sisu
-sisu = document.body; // body sisu
-sisu = document.doctype; // doctype
-sisu = document.domain;
-sisu = document.URL;
-sisu = document.characterSet;
-sisu = document.contentType;
-
-// formi andmed
-sisu = document.forms; // kõik vormid
-sisu = document.forms[0]; // esimene vorm
-sisu = document.forms[0].id; // esimese vormi id
-sisu = document.forms[0].method; // vormi töötlus meetod
-sisu = document.forms[0].action;
-
-// lingide andmed
-sisu = document.links; // kõik lingid
-sisu = document.links[0]; // esimene link
-sisu = document.links[0].className; // lingi klassid
-sisu = document.links[0].classList[1]; // klasside massiiv ja sealt elemendi lugemine
-
-// pildid
-sisu = document.images;
-
-// skriptid
-sisu = document.scripts;
-sisu = document.scripts[0].getAttribute('src'); // materialize js
-sisu = document.scripts[1].getAttribute('src'); // local js
+// stiili muutmine
+// sisu.style.background = '#333';
+document.getElementById('title').style.background = '#333';
+sisu.style.color = '#fff';
+sisu.style.padding = '5px';
+// sisu.style.display = 'none';
 console.log(sisu);
