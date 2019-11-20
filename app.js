acbfe36aa5a3ac505c2b5901357b86e45c2bbbd3
Anna Karutina <anna.karutina@khk.ee>
2019-11-20 09:23:59 +0200

    sündmused - sündmusobjekti omadused

--- app.js
+++ app.js
@@ -4,6 +4,22 @@ const kustutaKoik = document.querySelector('.clear-tasks');
 kustutaKoik.addEventListener('click', vajutaNupp);
 
 function vajutaNupp(e){
-  console.log('Vajutatud');
-  // e.preventDefault();
+  let sisu = e;
+  // target
+  sisu = e.target;
+  sisu = e.target.id;
+  sisu = e.target.className;
+  sisu = e.target.classList;
+
+  // tüüp
+  sisu = e.type;
+
+  // sündmuse kordinaadid browseri akna suhtes
+  sisu = e.clientY;
+  sisu = e.clientX;
+
+  // sündmuse kordinaadid elemendi enda suhtes
+  sisu = e.offsetY;
+  sisu = e.offsetX;
+  console.log(sisu);
 }
