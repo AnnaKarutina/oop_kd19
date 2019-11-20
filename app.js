6e0547fb69ba4443a8bcc553c12295d1d31a8c6b
Anna Karutina <anna.karutina@khk.ee>
2019-11-20 08:57:08 +0200

    sündmused - töötlus funktsioon ei ole sündmuse sees

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 // sündmused
 
 const kustutaKoik = document.querySelector('.clear-tasks');
-kustutaKoik.addEventListener('click', function(e){
+kustutaKoik.addEventListener('click', vajutaNupp);
+
+function vajutaNupp(e){
   console.log('Vajutatud');
   // e.preventDefault();
-});
+}
