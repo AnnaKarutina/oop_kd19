47466f595e601614a1e7783e1c166e5208bfefcf
Anna Karutina <anna.karutina@khk.ee>
2019-11-20 11:26:48 +0200

    sündmused - sisse ja väljapoole elementidest suunamine

--- app.js
+++ app.js
@@ -1,36 +1,30 @@
 // sündmused
 
-const form = document.querySelector('form');
-const lisaUusYlesanne = document.getElementById('task');
-const text = document.querySelector('h5');
+// bubbling
 
-// kustutame lisUusYlesanne väärtus
-lisaUusYlesanne.value = '';
+// lehe kaardi pealkiri
+// document.querySelector('.card-title').addEventListener('click', function(e){
+//   console.log('card-title');
+// });
 
-// klaviatuur
-// keydown
-// lisaUusYlesanne.addEventListener('keydown', syndmus);
-// keyup
-// lisaUusYlesanne.addEventListener('keyup', syndmus);
-// keypress
-// lisaUusYlesanne.addEventListener('keypress', syndmus);
-// focus
-// lisaUusYlesanne.addEventListener('focus', syndmus);
-// blur
-// lisaUusYlesanne.addEventListener('blur', syndmus);
-// paste
-// lisaUusYlesanne.addEventListener('paste', syndmus);
-// cut
-// lisaUusYlesanne.addEventListener('cut', syndmus);
-// input
-// lisaUusYlesanne.addEventListener('input', syndmus);
+// lehe kaardi sisu
+document.querySelector('.card-content').addEventListener('click', function(e){
+  console.log('card-content');
+});
 
+// delegeerimine
 
-function syndmus(e){
-  // logi sündmuse tüüp
-  console.log(`Sündmuse tüüp: ${e.type}`);
-  // kontrollime vormi kaudu saadetud väärtus
-  console.log(lisaUusYlesanne.value);
-  // väljastame ülesanne HTML-is
-  text.innerText = lisaUusYlesanne.value;
+const kustutaYlesanne = document.querySelector('.delete-item');
+kustutaYlesanne.addEventListener('click', kustuta);
+
+// console.log(kustutaYlesanne);
+
+function kustuta(e){
+   if(e.target.parentElement.classList.contains('delete-item')){
+    console.log('hakkame kustutama');
+    e.target.parentElement.parentElement.remove();
+  } 
 }
+
+
+
