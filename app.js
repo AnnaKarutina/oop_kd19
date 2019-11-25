095c2a7953ff5b8acc370c4ba09c921d561816f9
Anna Karutina <anna.karutina@khk.ee>
2019-11-25 14:54:16 +0200

    Raamatud - loodud raamat vormi andmetest

--- app.js
+++ app.js
@@ -1,32 +1,22 @@
-// inimese kirjeldus OOP abil
-// klasside pärilus
-class Isik {
-  // konstruktor
-  constructor(e, p){
-    this.eesnimi = e;
-    this.perenimi = p;
-  }
-  // tervitus meetod
-  tervitus() {
-    return `Tere, ${this.eesnimi} ${this.perenimi} !`;
-  }
+// Raamatu konstruktor
+function Raamat(a, p, i){
+  this.autor = a;
+  this.pealkiri = p;
+  this.isbn = i;
 }
 
-// kliendi klass
-class Klient extends Isik{
-  // konstruktor
-  constructor(e, p, t, s){
-    super(e, p);
-    this.telefon = t;
-    this.staatus = s;
-  }
+// kirjeldame raamatu lisamise sündmust
+document.getElementById('book-form').addEventListener('submit', lisaRaamat);
+// raamatu lisamise funktsioon
+function lisaRaamat(e){
+  // võtame andmed vormist
+  const pealkiri = document.getElementById('title').value;
+  const autor = document.getElementById('author').value;
+  const isbn = document.getElementById('isbn').value;
+  // loome raamat andmete põhjal
+  const raamat = new Raamat(pealkiri, autor, isbn);
 
-  // staatiline meetod
-  static kuutasu(){
-    return 5;
-  }
+  console.log(raamat);
+
+  e.preventDefault();
 }
-
-const kadi = new Klient('Kadi', 'Tamm', '1234 5678', 'hõbe');
-console.log(kadi);
-console.log(Klient.kuutasu());
