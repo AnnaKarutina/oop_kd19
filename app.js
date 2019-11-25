ef06fe4a9891012332b8629a18e6d0609759d4d5
Anna Karutina <anna.karutina@khk.ee>
2019-11-25 11:31:17 +0200

    Inimese kirjeldus OOP kujul

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
-// inimese kirjeldus
-let eesnimi, perenimi;
-let synnikuupaev;
+// inimese kirjeldus OOP abil
+// inimese konstruktor
+function Isik(e, p, skp){
+  this.eesnimi = e;
+  this.perenimi = p;
+  this.synnikuupaev = new Date(skp);
 
-function taisNimi(eesnimi, perenimi){
-  return `${eesnimi} ${perenimi}`;
+  // väljasta täisnimi
+  this.taisNimi = function(){
+    return `${this.eesnimi} ${this.perenimi}`;
+  }
+
+  // arvuta vanus
+  this.arvutaVanus = function(){
+    const vaheSekundites = Date.now() - this.synnikuupaev.getTime();
+    const vanusDateKujul = new Date(vaheSekundites);
+    const taisAasta = vanusDateKujul.getUTCFullYear();
+    const vanus = taisAasta - 1970;
+    return vanus;
+  }
 }
 
-function arvutaVanus(synnikuupaev){
-  synnikuupaev = new Date(synnikuupaev);
-  vaheSekundites = Date.now() - synnikuupaev.getTime();
-  vanusDate = new Date(vaheSekundites);
-  aastaDate = vanusDate.getUTCFullYear();
-  vanus = aastaDate - 1970;
-  return `Vanus: ${vanus} aastat`;
-}
-
-console.log(taisNimi("Minu", "Laps"));
-console.log(arvutaVanus("2014-08-01"));
+const anna = new Isik('Anna', 'Karutina', '11-27-1980');
+const kadi = new Isik('Kadi', 'Tamm', '07-30-1995');
+console.log(anna.taisNimi());
+console.log(kadi.taisNimi());
