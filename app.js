391049c2d8f9dfba65b2393ecc5015db9c3373c7
Anna Karutina <anna.karutina@khk.ee>
2019-11-25 11:49:02 +0200

    Objekti prototüübi kasutamine

--- app.js
+++ app.js
@@ -1,26 +1,25 @@
 // inimese kirjeldus OOP abil
+// Objekt.prototype
 // inimese konstruktor
 function Isik(e, p, skp){
   this.eesnimi = e;
   this.perenimi = p;
   this.synnikuupaev = new Date(skp);
-
-  // väljasta täisnimi
-  this.taisNimi = function(){
-    return `${this.eesnimi} ${this.perenimi}`;
-  }
-
-  // arvuta vanus
-  this.arvutaVanus = function(){
-    const vaheSekundites = Date.now() - this.synnikuupaev.getTime();
-    const vanusDateKujul = new Date(vaheSekundites);
-    const taisAasta = vanusDateKujul.getUTCFullYear();
-    const vanus = taisAasta - 1970;
-    return vanus;
-  }
+}
+// väljasta täisnimi
+Isik.prototype.taisNimi = function(){
+  return `${this.eesnimi} ${this.perenimi}`;
+}
+// arvuta vanus
+Isik.prototype.arvutaVanus = function(){
+  const vaheSekundites = Date.now() - this.synnikuupaev.getTime();
+  const vanusDateKujul = new Date(vaheSekundites);
+  const taisAasta = vanusDateKujul.getUTCFullYear();
+  const vanus = taisAasta - 1970;
+  return vanus;
 }
 
 const anna = new Isik('Anna', 'Karutina', '11-27-1980');
 const kadi = new Isik('Kadi', 'Tamm', '07-30-1995');
-console.log(anna.taisNimi());
+console.log(anna);
 console.log(kadi.taisNimi());
