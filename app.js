e0b1334d4b77c5298db84bba175458b638e4527b
Anna Karutina <anna.karutina@khk.ee>
2019-11-25 15:15:31 +0200

    Raamatud - raamatu lisamine tabelisse

--- app.js
+++ app.js
@@ -19,6 +19,21 @@ KL.prototype.puhastaSisend = function(){
   document.getElementById('isbn').value = '';
 }
 
+// raamatu lisamine tabelisse
+KL.prototype.lisaRaamatTabelisse = function(r){
+  // loome tabeli rida
+  const rida = document.createElement('tr');
+  // täidame rida tabeli andmetega
+  rida.innerHTML = `
+    <td>${r.pealkiri}</td>
+    <td>${r.autor}</td>
+    <td>${r.isbn}</td>
+  `;
+  // lisame rida tabelisse
+  tabel = document.getElementById('book-list');
+  tabel.appendChild(rida);
+}
+
 // kirjeldame raamatu lisamise sündmust
 document.getElementById('book-form').addEventListener('submit', lisaRaamat);
 // raamatu lisamise funktsioon
@@ -28,12 +43,13 @@ function lisaRaamat(e){
   const autor = document.getElementById('author').value;
   const isbn = document.getElementById('isbn').value;
   // loome raamat andmete põhjal
-  const raamat = new Raamat(pealkiri, autor, isbn);
-
-  console.log(raamat);
+  const raamat = new Raamat(autor, pealkiri, isbn);
 
   // loome kasutaja liidese objekt temaga opereerimiseks
   const kl = new KL();
+
+  // lisame sisestatud raamat tabelisse
+  kl.lisaRaamatTabelisse(raamat);
 
   // puhastame väljad sisestatud andmetest
   kl.puhastaSisend();
