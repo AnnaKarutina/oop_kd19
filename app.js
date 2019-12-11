9deeae58a04ad53ef645e0e0eeb28b0d0fb3da91
Anna Karutina <anna.karutina@khk.ee>
2019-12-11 15:39:34 +0200

    Lehe uuendamisega vaikimisi andmed on väljas

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const weather = new Weather('Tartu linn');
 // app UI
 const ui = new UI();
 
+// show default city weather
+document.addEventListener('DOMContentLoaded', drawWeather);
+
 // change city - weather
 const changeBtn = document.getElementById('w-change');
 changeBtn.addEventListener('click', changeWeather);
