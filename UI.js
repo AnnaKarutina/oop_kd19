c2cbc42522b04b255c00f92e33d3ac6e3496bac1
Anna Karutina <anna.karutina@khk.ee>
2019-12-11 15:18:58 +0200

    Lisatud niiskuse andmete näitamine

--- UI.js
+++ UI.js
@@ -3,6 +3,7 @@ class UI {
   constructor(){
     this.cityName = document.getElementById('w-city');
     this.temp = document.getElementById('w-temp');
+    this.humidity = document.getElementById('w-humidity');
     this.desc = document.getElementById('w-desc');
   }
 
@@ -10,6 +11,7 @@ class UI {
     this.cityName.textContent = cityWeather.name;
     const celcius = Math.round(parseFloat(cityWeather.main.temp)-273.15);
     this.temp.innerHTML = `${celcius} &deg;C`;
+    this.humidity.innerHTML = `${cityWeather.main.humidity} %`;
     this.desc.textContent = cityWeather.weather[0].description;
   }
 }
