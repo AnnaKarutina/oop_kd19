14c2d43c0dd6ab8e3d6bd96656f4588f359e6691
Anna Karutina <anna.karutina@khk.ee>
2019-12-18 11:45:08 +0200

    API anmete lugemine ja vigade kinni püüdmine

--- app.js
+++ app.js
@@ -1,5 +1,27 @@
 document.getElementById('btn1').addEventListener('click', getTextData);
 document.getElementById('btn2').addEventListener('click', getJsonData);
+document.getElementById('btn3').addEventListener('click', getJsonAPI);
+
+// remote API - remote json
+function getJsonAPI() {
+  fetch('https://api.github.com/users')
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    let out = '';
+    out = '';
+    data.forEach(function(users){
+      out = out + `<li>${users.login} `;
+      out = out + `<img src="${users.avatar_url}" width="50"></li>`;
+    });
+    document.getElementById('out').innerHTML = out;
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+}
 
 // local json file
 function getJsonData() {
