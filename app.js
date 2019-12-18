138c1104b300828d271ec31ee0ab9bf5bea2c223
Anna Karutina <anna.karutina@khk.ee>
2019-12-18 11:21:25 +0200

    JSON andmete lugemine

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 document.getElementById('btn1').addEventListener('click', getTextData);
+document.getElementById('btn2').addEventListener('click', getJsonData);
 
+// local json file
+function getJsonData() {
+  fetch('data.json')
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(data) {
+    console.log(data);
+    let out = '';
+    out = '';
+    data.forEach(function(timetable){
+      out = out + `<li>${timetable.opetaja} `;
+      out = out + `- ${timetable.aine}</li>`;
+      console.log(out);
+    });
+    document.getElementById('out').innerHTML = out;
+  })
+  .catch(function(error) {
+    console.log(error);
+  })
+}
+
+// local text file
 function getTextData() {
   fetch('text.txt')
   .then(function(res) {
@@ -13,3 +37,4 @@ function getTextData() {
     console.log(error);
   })
 }
+
